8d8beb4543d72debe28854ff2f59e266c271680c
Renuka Prasad <renukaprasad.kb@gmail.com>
2021-12-06 12:01:36 +0530

    second push

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -10,30 +10,38 @@ const verifyToken = (token) => {
     });
 };
 
-
 module.exports = async (req, res, next) => {
-
-    // check if the header has a bearer token
+    //? check if the header has a bearer token
     const bearerToken = req?.headers?.authorization;
 
-    // if the token received is not a bearer token, throw token
+    //? if the token received is not a bearer token, throw token
     if (!bearerToken || !bearerToken.startsWith('Bearer ')) {
-        return res.status(500).send({ message: "Please provide a valid token", status: "failed" });
+        return res.status(500).send({
+             message: "Please provide a valid token",
+              status: "failed" 
+            });
     }
 
-    // else get the user from the token
+    //? else get the user from the token
     const token = bearerToken.split(' ')[1];
     let user;
     try {
         user = await verifyToken(token);
-    } catch (e) {
-        return res.status(500).send({ message: "please provide a valid token", status: "failed" });
+    } 
+    catch (e) {
+        return res.status(500).send({ 
+            message: "please provide a valid token", 
+            status: "failed" 
+        });
     }
 
-    // if no user found, throw an err
-    if (!user) return res.status(500).send({ message: "please provide a valid token", status: "failed" });
+    //? if no user found, throw an err
+    if (!user) return res.status(500).send({ 
+        message: "please provide a valid token", 
+        status: "failed" 
+    });
 
-    // else append the user to req
+    //? else append the user to req
     req.body = {
         title: req.body.title,
         body: req.body.body,
@@ -41,6 +49,5 @@ module.exports = async (req, res, next) => {
     }
     console.log(req.body);
 
-    // return next
     return next();
 }
